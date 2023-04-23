9e8d1c16efc1a6af8c35b3b0ab70c1af52945817
jiyang <yang.ji@thoughtworks.com>
2023-04-23 15:14:50 +0800

    Revert "move logic to app"

--- server/app.py
+++ server/app.py
@@ -1,23 +1,3 @@
 from flask import Flask
-from server.website import Website
-from server.backend import Backend_Api
-from server.errors  import *
-
 
 app = Flask(__name__, template_folder='./../client/html')
-
-site = Website(app)
-for route in site.routes:
-    app.add_url_rule(
-        route,
-        view_func = site.routes[route]['function'],
-        methods   = site.routes[route]['methods'],
-    )
-
-backend_api  = Backend_Api(app)
-for route in backend_api.routes:
-        app.add_url_rule(
-            route,
-            view_func = backend_api.routes[route]['function'],
-            methods   = backend_api.routes[route]['methods'],
-        )
